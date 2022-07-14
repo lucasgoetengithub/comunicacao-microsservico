b782ee1c2d7cb3c2f90eeee0932286a20848f19b
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-13 21:08:59 -0300

    implementação authorization

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -25,7 +25,7 @@ class UserService {
             
         } catch (err) {
             return {
-                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
                 message: err.message,
             };
         }   
@@ -63,7 +63,7 @@ class UserService {
             }
         } catch (err) {
             return {
-                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
                 message: err.message,
             };
         }
