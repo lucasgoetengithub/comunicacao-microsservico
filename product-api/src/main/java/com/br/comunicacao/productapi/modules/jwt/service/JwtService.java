c5da7b583a8d89b1fbc13af3bfef6fcfe3adf865
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-20 17:02:35 -0300

    implementação jwt

--- product-api/src/main/java/com/br/comunicacao/productapi/modules/jwt/service/JwtService.java
+++ product-api/src/main/java/com/br/comunicacao/productapi/modules/jwt/service/JwtService.java
@@ -2,15 +2,12 @@ package com.br.comunicacao.productapi.modules.jwt.service;
 
 
 import com.br.comunicacao.productapi.config.exception.AuthenticationException;
-import com.br.comunicacao.productapi.config.exception.ValidationException;
 import com.br.comunicacao.productapi.modules.jwt.dto.JwtResponse;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.nio.charset.StandardCharsets;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -20,11 +17,13 @@ public class JwtService {
     @Value("${app-config.secrets.api-secret}")
     private String apiSecret;
 
-    private static String BEARER = "bearer";
+    private static String EMPTY_SPACE = " ";
+    private static Integer TOKEN_INDEX = 1;
 
     public void validateAuthorization(String token){
+        var accessToken = extractToken(token);
         try {
-            var accessToken = extractToken(token);
+
             var claims = Jwts
                     .parserBuilder()
                     .setSigningKey(Keys.hmacShaKeyFor(apiSecret.getBytes()))
@@ -46,9 +45,8 @@ public class JwtService {
             throw new AuthenticationException("The access token was not informed.");
         }
 
-        if (token.toLowerCase().contains(BEARER)){
-            token = token.toLowerCase();
-            token = token.replace(BEARER, Strings.EMPTY);
+        if (token.contains(EMPTY_SPACE)){
+            token = token.split(EMPTY_SPACE)[TOKEN_INDEX];
         }
 
         return token;
