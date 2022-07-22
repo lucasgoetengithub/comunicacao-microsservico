061fd0d18da8b653ed51f8f2272ea1141bac6334
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-22 00:34:03 -0300

    initial data sales-api

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,23 +1,16 @@
 import express from 'express';
 
 import { connect } from './src/config/db/mongoDbConfig.js';
-import Order from './src/modules/sales/model/Order.js';
+import { createInitialData } from './src/config/db/initialData.js';
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
 connect();
+createInitialData();
 
 app.get('/api/status', (req, res) => {
-    try {
-        let teste = Order.find();    
-        console.log(teste);
-        console.log("passou 123");
-    } catch (error) {
-       
-    }
-    
     
     return res.status(200).json({
         service: 'Sales-API',
