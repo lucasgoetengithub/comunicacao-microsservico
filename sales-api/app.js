fff603c06929adf12c6fc0fa8d9d84e2bb7ce23f
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-23 01:20:01 -0300

    criando sender

--- sales-api/app.js
+++ sales-api/app.js
@@ -5,6 +5,8 @@ import { createInitialData } from './src/config/db/initialData.js';
 import checkToken from './src/config/auth/checkToken.js';
 import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js';
 
+import { sendProductStockUpdateQueue } from './src/modules/sales/model/product/rabbitmq/productStockUpdate.js'; 
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
@@ -13,7 +15,29 @@ connectMongoDB();
 createInitialData();
 connectRabbitMq();
 
-app.use(checkToken);
+//app.use(checkToken);
+app.get('/teste', (req, res) => {
+    try {
+        sendProductStockUpdateQueue([
+            {
+                productId: 1001,
+                quantity: 3,
+            },
+            {
+                productId: 1002,
+                quantity: 2,
+            },
+            {
+                productId: 1003,
+                quantity: 1,
+            }
+        ])
+        return res.status(200).json({status:200});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:true});
+    }
+})
 
 app.get('/api/status', (req, res) => {
     
--- sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
+++ sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
@@ -1,12 +1,6 @@
 import amqp from 'amqplib/callback_api.js';
 
-import  { 
-    PRODUCT_STOCK_UPDATE_QUEUE, 
-    PRODUCT_STOCK_UPDATE_ROUTING_KEY, 
-    PRODUCT_TOPIC, 
-    SALES_CONFIRMATION_QUEUE, 
-    SALES_CONFIRMATION_ROUTING_KEY 
-} from '../../../config/rabbitmq/queue.js';
+import  { SALES_CONFIRMATION_QUEUE } from '../../../config/rabbitmq/queue.js';
 
 import { RABBIT_MQ_URL } from '../../../config/constants/secrets.js';
 
