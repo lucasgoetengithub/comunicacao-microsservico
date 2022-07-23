f1b53f7b9956fda68ea41d249d4217fe52d96c99
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-23 13:16:15 -0300

    criando processando assincrono de recebimento

--- sales-api/src/modules/sales/service/OrderService.js
+++ sales-api/src/modules/sales/service/OrderService.js
@@ -20,6 +20,9 @@ class OrderService {
                 products: orderData,
                 
             };
+
+            await this.validateProductStock(order);
+
             let createdOrder = await OrderRepository.save(order);
             sendProductStockUpdateQueue(createdOrder.products);
             return {
@@ -34,12 +37,40 @@ class OrderService {
         }
     }
 
+    async updateOrder(orderMessage){
+        try {
+            const order = JSON.parse(orderMessage);
+            if (!order.salesId && !order.status) {
+                let existingOrder = await OrderRepository.findById(order.salesId);
+                if (existingOrder && order.status !== existingOrder.status) {
+                    existingOrder.status = order.status;
+                    await OrderRepository.save(existingOrder);
+                }
+            } else {
+                console.warning('The order message was not complete.');
+            }
+        } catch (err) {
+            console.error('Could not parse order message from queue.');
+            console.error(err.message);
+        }
+    }
+
     validateOrderData(data){
         if (!data || !data.products) {
             throw new OrderException(BAD_REQUEST, 'The products must be informed.')
         }
 
-    } 
+    }
+
+    async validateProductStock(order){
+        let stockIsOut = true;
+        if (stockIsOut) {
+            throw new OrderException(
+                BAD_REQUEST,
+                'The stock is out for the products.'
+            ); 
+        }
+    }
 }
 
 export default new OrderService();
